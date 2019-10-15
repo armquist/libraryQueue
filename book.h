11a7bf1c400de7c58575e77ddee94f7a1f0202b5
AhmedBoukhousse <47191893+AhmedBoukhousse@users.noreply.github.com>
2019-10-15 16:15:39 -0500

    Update book.h

--- book.h
+++ book.h
@@ -1,1 +1,22 @@
-#pragma once
+
+#ifndef Booklist_h
+#define Booklist_h
+#include <iostream>
+#include <vector>
+#include "Employee"
+#include "Date.h"
+
+template <typename T>
+class Book{
+public:
+    Date start_date;
+    Date end_date;
+   void get_start_date (const &String date, DateFormat format);
+    void get_end_date (const $String date, DateFormat format);
+    void pop();
+    void push(const T&the_data);
+private:
+    Queue <T> employees;
+    T name;
+};
+#endif /* Booklist_h */
