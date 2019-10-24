80dd5c392b3ee03620fc46fa1dcc32993b7c6827
AhmedBoukhousse <47191893+AhmedBoukhousse@users.noreply.github.com>
2019-10-24 14:01:58 -0500

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -1,24 +1,107 @@
-#include "stdafx.h"
-#include "employee.h"
-#include "book.h"
-#include "library.h"
 
-using namespace std;
+//----------------------
+#include "library.hpp"
 
-void add_employee(string name){
-  queue<string>waitlist;
-  waitlist.push(name);
-}
-
-void add_book(string bookName) {
-  queue<string>booklist;
-  booklist.push(bookName);
-}
-
-void pass_on() {
+Library::Library(){
 
 }
 
-void circulate_book(){
+Library::~Library(){
+
+list<Employee*>::iterator it;
+
+for (it = employeeList.begin(); it != employeeList.end(); it++){
+
+delete *it;
 
 }
+
+}
+
+void Library::add_book(string newBook){
+
+Book tempBook(newBook);
+
+toBeCirculated.push_back(tempBook);
+
+}
+
+void Library::add_employee(string name){
+
+Employee *temp;
+
+temp = new Employee(name);
+
+employeeList.push_front(temp);
+
+}
+
+void Library::circulate_book(string bookToMove, Date dayOfMove){
+
+list<Book>::iterator it;
+
+for (it = toBeCirculated.begin(); it != toBeCirculated.end(); it++){ // find book : O(n)
+
+if (it->getname() == bookToMove){ // this is the book
+
+it->populate_queue(employeeList); // create queue
+
+it->setstartDate(dayOfMove); // save start date
+
+break;
+
+}
+
+}
+
+cerr << "Book not found" << endl;
+
+}
+
+void Library::pass_on(string bookToMove, Date date){
+
+Employee *next, *prev; // prev is employee that is popped
+
+list<Book>::iterator it;
+
+for (it = toBeCirculated.begin(); it != toBeCirculated.end(); it++){ // find book : O(n)
+
+if (it->getname() == bookToMove){
+
+prev = it->pop_max(); // pop max, save to prev
+
+prev->retain(date - it->getHeld()); // retain = current date - last pass of book
+
+if (!it->isEmpty()){
+
+next = it->top(); // next is now max
+
+next->wait(date - it->getstartDate()); // wait = current date - book start date
+
+it->setHeld(date); // save pass date for next employee
+
+}
+
+else{ // circulation complete
+
+it->setarchived(true);
+
+it->setendDate(date);
+
+archived.push_back(*it); // archive
+
+toBeCirculated.erase(it); // erase
+
+}
+
+break;
+
+}
+
+}
+
+cerr << "Book not found" << endl;
+
+}
+
+
