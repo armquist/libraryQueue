ed18d8988ad7f07f0b4e13094826dc8b40826b38
ayrahca007 <55465855+ayrahca007@users.noreply.github.com>
2019-10-31 16:59:28 -0500

    finishing up
    documenting library class

--- library.h
+++ library.h
@@ -7,7 +7,6 @@
 #include "employee.h"
 #include "book.h"
 #include <string>
-#include <queue>
 #include <list>
 #include "date.h"
 
@@ -15,18 +14,18 @@ using namespace std;
 
 class library {
 public:
-	library();
-//	~library();
+	library(); //constructor
+	~library();//destructor
 
-	void add_employee(string name);
-	void add_book(string bookName);
-	void pass_on(string bookName, Date& book_pass);
-	void circulate_book(string bookName, Date& date_cir);
+	void add_employee(string name); // adds the name of the employee
+	void add_book(string bookName); // adds the book
+	void pass_on(string bookName, Date& book_pass); // passes the book from one employee to another
+	void circulate_book(string bookName, Date& date_cir); // Puts the book into circulation
 
 private:
-	list <book> books_archived;
-	list <book> books_circulated;
-	list <employee*> employees;
+	list <book> books_archived; // list of books at the end of circulation 
+	list <book> books_circulated; // list of books in circulation
+	list <employee*> employees; // list of the employees
 };
 
 #endif
