3c7e5b4cf7de322323f26df52f909539254547db
armquist <tstarr.timberlake@gmail.com>
2019-10-31 17:07:42 -0500

    Finish up book class
    book class documented, priorityQueue operation implemented thanks to Ahmed

--- book.cpp
+++ book.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-book::book(const string& bookName) {
+book::book(const string& bookName) { //Constructor if appropriate arguments are entered
 	name = bookName;
 	archived = false;
 }
 
-book::book() {
+book::book() {  //Constructor if no arguments are entered
 	name = "none";
 	start_date = Date(2000, 1, 1, DateFormat::US);
 	end_date = Date(2000, 1, 1, DateFormat::US);
@@ -17,56 +17,54 @@ book::book() {
 	archived = false;
 }
 
-book::~book() {
+book::~book() { //Destructor
 
 }
 
-void book::addToCirculation(const Date& date) {
-	start_date = date;
-	lastPass = date;
-}
-
-string book::getName() {
+string book::getName() { //will return name of book
 	return name;
 }
 
-void book::add_employee(employee*& personnel) {
-	waitingEmployees.push_back(personnel);
+void book::pushToQueue(employee*& personnel) {  //will push employee to priority queue O(1)
+	theQueue.push(personnel);
 }
 
-void book::setArchived(const Date& date) {
+void book::setArchived(const Date& date) { //will set archive flag on book and set end date for book
 	archived = true;
 	end_date = date;
 }
 
-void book::passOn(const Date& passDate) {
-	waitingEmployees.front()->setRetainTime(passDate, lastPass);
-	waitingEmployees.pop_front();
-	if (waitingEmployees.empty()) {
+void book::passOn(const Date& passDate) { //will finish book passing using elements from within the class: O(1)
+	employee *mostNeeded; //highest priority employee
+	employee *otherEmployee; //employee at top after mostNeeded gets popped
+	cout << "Book " << name << " has been passed from " << theQueue.get_top()->getName(); //user feedback
+	mostNeeded = pop_max(); //pos highest priority item
+	mostNeeded->setRetainTime(passDate, lastPass); //sets the time user had book
+	if (theQueue.isEmpty()) { //pass to archives once queue is empty
 		setArchived(passDate);
+		cout << " to " << "the Archives" << " on " << end_date << endl;
 	}
-	else {
-		waitingEmployees.front()->setWaitTime(start_date, passDate);
+	else {	//get employee at top of queue and set their wait time, set the time the book was passed
+		otherEmployee = get_top();
+		otherEmployee->setWaitTime(start_date, passDate);
 		lastPass = passDate;
+		cout << " to " << theQueue.get_top()->getName() << " on " << lastPass << endl; //user feedback
 	}
 }
 
-void book::set_start_date(Date theDate) {
+void book::set_start_date(Date theDate) { //will set the date that the book is circulated
 	start_date = theDate;
 	lastPass = theDate;
 }
-void book::set_end_date(Date theDate) {
-	end_date = theDate;
-}
 
-Date book::get_start_date() {
-	return start_date;
-}
-
-Date book::get_end_date() {
-	return end_date;
-}
-
-bool book::getArchived() {
+bool book::getArchived() { //will get archive flag
 	return archived;
+}
+
+employee* book::pop_max() { //function inherited from prorityQueue
+	return theQueue.pop_max();
+}
+
+employee* book::get_top() { //function inherited from prorityQueue
+	return theQueue.get_top();
 }
--- book.h
+++ book.h
@@ -6,29 +6,28 @@
 #include <string>
 #include "Employee.h"
 #include "Date.h"
+#include "priorityQueue.h"
 
 class book {
 public:
-	book();
-	book(const string& bookName);
-	~book();
-	void addToCirculation(const Date& date);
-	void setArchived(const Date& date);
-	Date get_start_date();
-	void set_start_date(Date theDate);
-	void set_end_date(Date theDate);
-	Date get_end_date();
-	string getName();
-	void add_employee(employee*& personnel);
-	void passOn(const Date& passDate);
-	bool getArchived();
+	book(); //Constructor if no arguments are entered
+	book(const string& bookName); //Constructor if appropriate arguments are entered
+	~book(); //destructor
+	void setArchived(const Date& date); //will set archive flag on book and set end date for book
+	void set_start_date(Date theDate); //will set the date that the book is circulated
+	string getName(); //will return name of book
+	void pushToQueue(employee*& personnel); //will push employee to priority queue
+	void passOn(const Date& passDate); //will finihs book passing using elements from within the class
+	bool getArchived(); //will get archive flag
+	employee* pop_max(); //function inherited from prorityQueue
+	employee* get_top(); //function inherited from prorityQueue
 
 private:
-	list<employee*> waitingEmployees;
 	string name;
 	bool archived;
 	Date start_date;
 	Date end_date;
-	Date lastPass;
+	Date lastPass; //last time book was passed
+	priorityQueue theQueue; //the priority q6ue6u6e defined for the book
 };
 #endif /* Booklist_h */
