6dc2318cddb2341bd65b5a919ecfda3bd6f4a64d
Ozzie-bit <55465895+Ozzie-bit@users.noreply.github.com>
2019-10-31 16:04:31 -0500

    Update employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -4,35 +4,36 @@
 
 using namespace std;
 
+//getting employee information to initiate
 employee::employee() {
 	name = "none";
 	waitTime = 0;
 	retainTime = 0;
 }
 
+// intitializing employee information for employee information
 employee::employee(string theName) {
 	name = theName;
 	waitTime = 0;
 	retainTime = 0;
 }
 
-int employee::getRetainTime() {
-	return retainTime;
-}
-
-int employee::getWaitTime() {
-	return waitTime;
-}
-
+//calculating wait time for employee
 void employee::setWaitTime(Date firstDate, Date currentDate) {
 	waitTime = currentDate - firstDate;
 }
 
+//calculating the retain time for employee
 void employee::setRetainTime(Date currentDate, Date lastPass) {
 	retainTime = currentDate - lastPass;
 }
 
+//setting the priority for employee based on wait and retain time calculation
 int employee::getPriority() {
 	return waitTime - retainTime;
 }
 
+//making employee name available
+string employee::getName() {
+	return name;
+}
