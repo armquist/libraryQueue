a2675b2253e11f840d94a6500f6dab1513595d85
Ozzie-bit <55465895+Ozzie-bit@users.noreply.github.com>
2019-10-31 16:14:08 -0500

    employee cleanup
    Commented and cleaned up employee class, less spaghetti code to deal with

--- employee.h
+++ employee.h
@@ -6,22 +6,33 @@
 #include "stdafx.h"
 #include "date.h"
 #include <string>
-#include <queue>
 
 using namespace std;
 
 class employee {
 private:
+	//privatizing specific employee information
 	string name;
 	int waitTime;
 	int retainTime;
 public:
+	//getting and setting the employee
 	employee();
 	employee(string theName);
+	
+	//calculating the wait time for employee
 	int getWaitTime();
+	
+	//calculating the retian for employee
 	int getRetainTime();
+	
+	//calculating retain time based on our date class
 	void setRetainTime(Date currentDate, Date lastPass);
+	
+	//calculating our wait time based on our date class
 	void setWaitTime(Date firstDate, Date currentDate);
+	
+	//setting the priority based on the wait and retain time
 	int getPriority();
 };
 
